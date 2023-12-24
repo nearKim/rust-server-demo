86beaefa982fb55ce9a63cce3b56ae778d19a34a
nearkim <garfield@snu.ac.kr>
2023-12-24 23:39:02 +0900

    ongoing

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod server;
+mod http;
+
+
+use server::Server;
+use http::Request;
+use http::Method;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
