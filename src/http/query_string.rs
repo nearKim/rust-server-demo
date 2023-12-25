1596b2667497e2ed135413c140b951c4d1e3d024
nearkim <garfield@snu.ac.kr>
2023-12-26 07:34:59 +0900

    ongoing

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::http::query_string::Value::Single;
 
 pub struct QueryString<'buf> {
     data: HashMap<&'buf str, Value<'buf>>
@@ -12,5 +13,35 @@ pub enum Value<'buf> {
 impl <'buf> QueryString<'buf> {
     pub fn get(&self, key: &str) -> Option<&Value<'buf>> {
         self.data.get(key)
+    }
+}
+
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(s: &'buf str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&') {
+            let mut key = sub_str;
+            let mut val = "";
+
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                val = &sub_str[i+1..]
+            }
+
+            data.entry(key).and_modify(|existing: &mut Value| match existing {
+                Value::Single(prev_val) => {
+                    // 아래 3줄과 vec!는 같은 일을 한다
+                    // let mut vec = Vec::new();
+                    // vec.push(val);
+                    // vec.push(prev_val);
+                    // existing은 주소이기 때문에 그것이 가리키는 주소의 HashMap을 swap하기 위해서는 dereference가 필요
+                    *existing = Value::Multiple(vec![prev_val, val]);
+                }
+                Value::Multiple(vec) => {vec.push(val)}
+            }).or_insert(Value::Single(val));
+        }
+
+        QueryString { data }
     }
 }
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::{Method, MethodError};
+use super::{QueryString};
 use std::str::Utf8Error;
 use std::convert::TryFrom;
 use std::error::Error;
@@ -7,7 +8,7 @@ use std::str;
 
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -60,7 +61,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
         // }
 
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i];
         }
 
